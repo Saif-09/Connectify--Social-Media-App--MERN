a91c7c6a8f967befe8322bb4d76d6ca111bffbbd
Saif-09 <saifmd238@gmail.com>
2023-09-21 01:08:36 +0530

    friend list and  suggested friends comleted

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
 import ProfileCard from "./ProfileCard";
+import FriendsCard from "./FriendsCard";
 
-export {InputText, Loading, CustomButton, NavBar, ProfileCard};
+export {InputText, Loading, CustomButton, NavBar, ProfileCard, FriendsCard};
