375fb22f94fe32367c484ca128b8737dbb54d3f4
Saif-09 <saifmd238@gmail.com>
2023-09-20 19:36:00 +0530

    left side div is in progress

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import InputText from "./InputText";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
+import ProfileCard from "./ProfileCard";
 
-export {InputText, Loading, CustomButton, NavBar};
+export {InputText, Loading, CustomButton, NavBar, ProfileCard};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useSelector} from "react-redux"
-import { NavBar } from '../components'
+import { NavBar, ProfileCard } from '../components'
 
 
 const Home = () => {
@@ -9,6 +9,18 @@ const Home = () => {
   return (
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
       <NavBar/>
+
+      <div className='w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full'>
+        {/* left */}
+        <div className='hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto'>
+
+          <ProfileCard user={user}/>
+        </div>
+        {/* center */}
+        <div></div>
+        {/*         right  */}
+        <div></div>
+      </div>
       
     </div>
   )
