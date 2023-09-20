6e3e036163e1b281d7d09da78d30ac0f0e75557e
Saif-09 <saifmd238@gmail.com>
2023-09-20 12:15:49 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Home, Login, Profile, Register, ResetPassword } from './pages';
 
+const Layout = () => {
+  const user = null;
 
+  //getting the current path 
+  const location = useLocation()
+
+  //if the user have token and is authenticated then it renders the Child components and if user gives false then it will redirect the user to login page
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
 
 function App() {
-  
+
 
   return (
     <>
-    <div className='w-full min-h-[100vh]'>
-      <p className='text-blue'>Hello</p>
-    </div>
-     
+      <div className='w-full min-h-[100vh]'>
+        <Routes>
+        //These will be accessed only when user is logged in
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile/:id?' element={<Profile />} />
+
+          </Route>
+
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+
+        </Routes>
+      </div>
+
     </>
   )
 }
