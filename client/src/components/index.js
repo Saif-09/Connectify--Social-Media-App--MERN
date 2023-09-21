42392399d6466f74be98926fda9e20a468dbed19
Saif-09 <saifmd238@gmail.com>
2023-09-21 11:20:32 +0530

    posts section done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,6 @@ import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
 import ProfileCard from "./ProfileCard";
 import FriendsCard from "./FriendsCard";
+import PostCard from "./PostCard";
 
-export {InputText, Loading, CustomButton, NavBar, ProfileCard, FriendsCard};
+export {InputText, Loading, CustomButton, NavBar, ProfileCard, FriendsCard, PostCard};
