5415c847fabca722f58ddec0cd9272c68a0e7df0
Saif-09 <saifmd238@gmail.com>
2023-09-23 04:08:37 +0530

    profile functionality added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,20 +2,44 @@ import React,{useState} from 'react'
 import { useDispatch, useSelector } from "react-redux" ;
 import { useParams } from "react-router-dom";
 import { FriendsCard, Loading, NavBar, PostCard, ProfileCard } from '../components';
-import { posts } from '../assets/data';
+import { useEffect } from 'react';
+import { deletePost, fetchPosts, getUserInfo, likePost } from '../utils';
+
 
 const Profile = () => {
 
   const { id }= useParams()
   const dispatch = useDispatch();
   const {user} = useSelector( (state) => state.user)
-  // const {posts} = useSelector( (state) => state.posts)
+  const {posts} = useSelector( (state) => state.posts)
   const [userInfo, setUserInfo] = useState(user);
   const [loading, setLoading] = useState(false);
   const uri = "/posts/get-user-post/" + id;
 
-  const handleDelete =()=>{}
-  const handleLikePost =()=>{}
+  const getUser = async()=>{
+    const res = await getUserInfo(user?.token, id);
+    setUserInfo(res);
+  }
+
+  const getPosts = async()=>{
+    await fetchPosts(user.token, dispatch, uri);
+    setLoading(false);
+  }
+
+  const handleDelete = async(id)=>{
+    await deletePost(id, user.token);
+    await getPosts()
+  }
+  const handleLikePost = async(uri)=>{
+    await likePost({uri:uri, token:user?.token});
+    await getPosts();
+  }
+
+  useEffect(()=>{
+    setLoading(true);
+    getUser();
+    getPosts();
+  },[id]);
 
   return (
     <>
